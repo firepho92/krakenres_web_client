f4b0b199c51e252edd3a4d0fe4521340ecf61127
Alexandro Aguilar <auza920205@gmail.com>
2017-10-19 15:16:18 -0500

    icon files reorganized

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
-import {Row, Col, Container, Form, FormGroup} from 'reactstrap'
+import {Row, Col, Container, Form, FormGroup, Button} from 'reactstrap'
 import './Login.css'
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log(this.inputA.value + " " + this.inputB.value);
+    event.preventDefault();
+  }
 
   render() {
     return (
@@ -10,22 +19,23 @@ class Login extends Component {
         <Container>
           <Row className="justify-content-center login-container">
             <Col className="align-self-center" md={{size: 6}}>
-              <Form className="login-form">
+              <Form className="login-form" onSubmit={this.handleSubmit}>
                 <h2>Entrar</h2>
                 <FormGroup>
                   <label className="sr-only" for="inlineFormInputGroup">Usuario</label>
                   <div className="input-group mb-2 mb-sm-0">
                     <div className="input-group-addon"><i className="fa fa-user-o" aria-hidden="true"></i></div>
-                    <input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Username" />
+                    <input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Usuario" ref={(input) => this.inputA = input}/>
                   </div>
                 </FormGroup>
                 <FormGroup>
                   <label className="sr-only" for="inlineFormInputGroup">Contraseña</label>
                   <div className="input-group mb-2 mb-sm-0">
                     <div className="input-group-addon"><i className="fa fa-lock" aria-hidden="true"></i></div>
-                    <input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Username" />
+                    <input type="password" className="form-control" id="inlineFormInputGroup" placeholder="Contraseña" ref={(input) => this.inputB = input} />
                   </div>
                 </FormGroup>
+                <Button outline className="submit-btn">Entrar</Button>
               </Form>
             </Col>
           </Row>
